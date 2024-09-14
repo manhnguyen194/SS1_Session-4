bf6b40b1a130d3f4867ae48ade4c5822bc410545
han8129 <110092300+han8129@users.noreply.github.com>
2024-09-14 23:07:50 +0700

    Add "TODO" to Main.java

--- src/main/java/vn/edu/hanu/fit/ss1/group1/session4/armRace/Main.java
+++ src/main/java/vn/edu/hanu/fit/ss1/group1/session4/armRace/Main.java
@@ -1,4 +1,7 @@
 package vn.edu.hanu.fit.ss1.group1.session4.armRace;
 
 public class Main {
+    // TODO initialize the Problem
+    // TODO run the Algorithm with the Problem passed in
+    // TODO print the result
 }
